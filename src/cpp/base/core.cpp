2385856c0bfbe8857cea0c758951ffbea8f8a67c
Vladimir Zarypov <krre31@gmail.com>
2015-12-28 11:50:50 +0200

    added functions for file and url processing to Core class

--- src/cpp/base/core.cpp
+++ src/cpp/base/core.cpp
@@ -4,3 +4,20 @@ Core::Core(QObject* parent) : QObject(parent)
 {
 
 }
+
+QString Core::pathToBaseName(const QString& path)
+{
+    QFileInfo fileInfo(path);
+    return fileInfo.baseName();
+}
+
+bool Core::isFileExists(const QString& filePath)
+{
+    QFileInfo checkFile(filePath);
+    return checkFile.exists() && checkFile.isFile();
+}
+
+bool Core::removeFile(const QString& filePath)
+{
+    return QFile::remove(filePath);
+}
--- src/cpp/base/core.h
+++ src/cpp/base/core.h
@@ -6,10 +6,18 @@ class Core : public QObject
     Q_OBJECT
     Q_PROPERTY(QString buildDate READ buildDate CONSTANT)
     Q_PROPERTY(QString qtVersion READ qtVersion CONSTANT)
+    Q_PROPERTY(QString homePath READ homePath CONSTANT)
 
 public:
     explicit Core(QObject* parent = 0);
     QString buildDate() { return QString(__DATE__); }
     QString qtVersion() { return QT_VERSION_STR; }
+    Q_INVOKABLE QString urlToPath(const QUrl& url) { return url.toLocalFile(); }
+    Q_INVOKABLE QString urlToFileName(const QUrl& url) { return url.fileName(); }
+    Q_INVOKABLE QUrl pathToUrl(const QString& path) { return QUrl::fromLocalFile(path); }
+    Q_INVOKABLE QString pathToBaseName(const QString& path);
+    QString homePath() { return QStandardPaths::writableLocation(QStandardPaths::HomeLocation); }
+    Q_INVOKABLE bool isFileExists(const QString& filePath);
+    Q_INVOKABLE bool removeFile(const QString& filePath);
 
 };
