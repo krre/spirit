668ce4310ac3d95fb9a6dbd874305909d04a5ef9
Vladimir Zarypov <krre31@gmail.com>
2016-01-22 14:04:52 +0200

    refactored Utils.createDynamicObject() function

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,11 @@
+.import QtQuick 2.6 as QtQuick
+
 function createDynamicObject(parent, url, properties) {
     var component = Qt.createComponent(url)
-    var errorMessage = component.errorString()
-    if (errorMessage) {
-        print(errorMessage)
-    } else {
+    if (component.status === QtQuick.Component.Ready) {
         return component.createObject(parent, properties || {})
+    } else {
+        print(component.errorString())
     }
 }
 
